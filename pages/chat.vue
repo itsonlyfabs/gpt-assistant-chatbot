c27da9077289be9f10bf7edb87a17c27fb640481
Fabio Ponzoni <ponzonif@gmail.com>
2025-04-14 20:36:13 +1200

    Fix: split login and chat pages

--- pages/chat.vue
+++ pages/chat.vue
@@ -4,11 +4,11 @@
 
 <script setup>
 import { supabase } from '@/utils/supabaseClient'
-import ChatComponent from '@/components/ChatComponent.vue' // we move your full chat here
+import ChatComponent from '@/components/ChatComponent.vue'
 
 const { data: { user } } = await supabase.auth.getUser()
 
 if (!user) {
-  navigateTo('/') // Force to login if not logged in
+  navigateTo('/') // redirect to login if not logged in
 }
 </script>
