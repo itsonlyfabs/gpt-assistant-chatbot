9420fde263fe7922b8b8eedec2f5cb4487af67c1
Fabio Ponzoni <ponzonif@gmail.com>
2025-04-16 11:20:43 +1200

    ✅ Add missing env vars to runtimeConfig

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
- css: ['~/assets/css/main.css'],
+  css: ['~/assets/css/main.css'],
 
- runtimeConfig: {
-					OPENAI_API_KEY: process.env.OPENAI_API_KEY
-	},
+  runtimeConfig: {
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    OPENAI_ASSISTANT_ID: process.env.OPENAI_ASSISTANT_ID,
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_SERVICE_KEY: process.env.SUPABASE_SERVICE_KEY,
+  },
 
- postcss: {
-					plugins: {
-									tailwindcss: {},
-									autoprefixer: {}
-					}
-	},
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {}
+    }
+  },
 
- compatibilityDate: '2025-04-15'
-});
+  compatibilityDate: '2025-04-15'
+})
