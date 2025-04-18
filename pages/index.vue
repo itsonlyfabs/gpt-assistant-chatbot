6b99e4c5b5316d257daf34f7d54c045b73591fff
Fabio Ponzoni <ponzonif@gmail.com>
2025-04-18 19:30:27 +1200

    mobile responsiive

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="flex flex-col items-center justify-center min-h-screen py-20 text-center bg-gradient-to-b from-[#3f4c6b] to-[#606c88]">
-    <div class="mb-16">
-      <h1 class="text-7xl font-bold mb-8 text-white">Life<br>Accelerator</h1>
-      <p class="text-lg max-w-md mx-auto opacity-80 mb-16 text-zinc-200">Get instant guidance and clarity for your life's challenges</p>
+    <div class="mb-16 px-4 max-w-full">
+      <h1 class="text-5xl md:text-7xl font-bold mb-6 md:mb-8 text-white">Life<br>Accelerator</h1>
+      <p class="text-base md:text-lg max-w-md mx-auto opacity-80 mb-10 md:mb-16 text-gray-200">Get instant guidance and clarity for your life's challenges</p>
       <button
         @click="loginWithGoogle"
-        class="px-8 py-4 rounded-full bg-gradient-to-r from-indigo-400 to-pink-400 text-white font-medium hover:opacity-90 transition flex items-center justify-center mx-auto"
+        class="px-6 md:px-8 py-3 md:py-4 rounded-full bg-gradient-to-r from-indigo-400 to-pink-400 text-white font-medium hover:opacity-90 transition flex items-center justify-center mx-auto"
       >
         <span>Login with Google</span>
       </button>
